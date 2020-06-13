28e38145663ce1663783c25f1d4e020c06cba713
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-06-13 14:48:40 -0400

    Create fundamentals.py

---
+++ fundamentals.py
@@ -0,0 +1,86 @@
+# FUNDAMENTALS OF PYTHON
+
+'''
+1 Syntax
+2 Operators & Operands
+3 Statments & Conditions
+4 Strings
+5 Comments
+'''
+
+########################################################################################################################################
+
+
+# SYNTAX - Values and Variables
+
+x = 10
+y = "ten"
+z = x + 5
+
+print(x, y, z)
+
+
+########################################################################################################################################
+
+
+# OPERATORS & OPERANDS
+
+
+#operatiors are icons for coomputation
+# + is addition
+# - is subtraction
+# * is multiplication
+# ** is to the power of
+# / is division
+# // is floor division
+# = is assignment
+# == is check if equal
+
+#Python follows PEMDAS
+
+
+########################################################################################################################################
+
+
+# STATEMENTS & CONDITIONS
+
+
+#if statement
+
+#else statement
+
+#if-else statement
+
+#while loop
+
+#do-while loop
+
+#for loop
+
+
+
+########################################################################################################################################
+
+
+# STRINGS
+
+
+word = "I am a string"
+
+word2 = "hi "
+
+word3 = word2 + word
+
+print(word3 * 2)
+
+
+########################################################################################################################################
+
+
+#COMMENTS
+
+# single line comment
+
+#** 
+  multi-line comment
+**#
