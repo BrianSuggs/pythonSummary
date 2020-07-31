a1facfe1ccb3c432cb0cc164c1aef8499615b8a7
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-07-31 18:21:34 -0400

    Update datastruct.py

--- datastruct.py
+++ datastruct.py
@@ -1,6 +1,20 @@
-'''
-OBJECTS
-
-
+# DATA STRUCTURES OF PYTHON
 
 '''
+1 Objects
+2 Class
+3 List
+4 Array
+5 Tuples
+6 Dictionaires
+7 Stack
+8 Queues
+9 Linked List
+10 Trees
+11 Matrix
+'''
+
+########################################################################################################################################
+
+
+# OBJECTS
