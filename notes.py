f1022dadab1f9aae7898a1d4825b967e4049598b
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-07-31 18:39:44 -0400

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,13 @@
+# NOTES OF PYTHON
+
+'''
+1 Rules of the language
+2 Strengths
+3 Limitations
+4 Pratical applications
+'''
+
+########################################################################################################################################
+
+
+# RULES OF THE LANGUAGE
