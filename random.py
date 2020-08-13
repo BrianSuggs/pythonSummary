d841c791472e267c38d06b423e5e775c06427ac5
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-08-13 17:48:18 -0400

    Update random.py

--- random.py
+++ random.py
@@ -1,20 +1,21 @@
 # How to generate random numbers
-
 import random
 
-
+# generates random number from 1-10
 print(random.randrange(1, 10))
 
-valid = false
 
-while valid == false:
+valid = False
+
+while valid == False:
   low = int(input('Enter lower bound number '))
   high = int(input('Enter upper bound number '))
   
   if low < high:
-    valid = true
+    valid = True
   
   if low > high:
     print('Invalid parameters ')
 
+# generates random number between low and high
 print(random.randrange(low, high))
