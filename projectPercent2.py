3022e0783c1e36233a1e23a8e6c70a40d4162f4c
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-08-17 18:50:27 -0400

    Update projectPercent2.py

--- projectPercent2.py
+++ projectPercent2.py
@@ -1,32 +1,47 @@
-# Percentage finder
+# Percentage finder PART 2
+
 # finds the percentage difference between two numbers
-
 def ptEval(startNum, endNum):
-    #endNum = float(input('Next number '))
-
     numDiff = endNum - startNum
     percent = (numDiff / startNum) * 100
     percent = round(percent, 2)
 
-    print('Start: ', startNum, '\t End: ', endNum)
-
     if numDiff < 0:
-        print('Account decreased by ', percent, '%')
+        print('Start: ', startNum, '\t End: ', endNum,
+              '\t', percent, '%')
     
     if numDiff > 0:
-        print('Account increased by ', percent, '%')
-    
+        print('Start: ', startNum, '\t End: ', endNum,
+              '\t +', percent, '%')
+
     return endNum
 
 
+# Finds the percentage change from a sequence of numbers while using a list
+def percentList():
+    entryNum = int(input('How many entries? '))
+    numList = []
+
+    for x in range(entryNum):
+        entry = float(input('Enter number '))
+        numList.append(entry)
+
+        if len(numList) > 1:
+            ptEval(numList[x-1], numList[x])
+
+    # prints the percentage change from first and last entry
+    print('\n')
+    ptEval(numList[0], numList[x])
+
+
+# Finds the percentage change from a sequence of numbers
 def main():
     entryNum = int(input('How many entries? '))
     startNum = float(input('Enter the inital number '))
-    endNum = float(input('Next number '))
     
     for x in range(entryNum):
-      startNum = ptEval(startNum, endNum)
-      
+        endNum = float(input('Next number '))
 
-
+        # the new starNum is the old endNum
+        startNum = ptEval(startNum, endNum)
 
