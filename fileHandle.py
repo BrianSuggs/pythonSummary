8ea36c7ab72efc953400809f035eb57dc0339c77
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-08-25 19:30:27 -0400

    Update fileHandle.py

--- fileHandle.py
+++ fileHandle.py
@@ -26,3 +26,72 @@
 
 """
 
+
+# Syntax
+# To open a file for reading it is enough to specify the name of the file
+f = open("demofile.txt")
+
+# The code above is the same as
+# Because "r" for read, and "t" for text are the default values, you do not need to specify them
+f = open("demofile.txt", "rt")
+
+
+######################################################################################################################################################
+
+# READ FILES
+
+# Open a File on the Server
+# Assume we have the following file, located in the same folder as Python
+# The open() function returns a file object, which has a read() method for reading the content of the file
+f = open("demofile.txt", "r")
+print(f.read())
+
+# If the file is located in a different location, you will have to specify the file path, like this
+f = open("D:\\myfiles\welcome.txt", "r")
+print(f.read())
+
+# Read Only Parts of the File
+# By default the read() method returns the whole text, but you can also specify how many characters you want to return
+# Return the 5 first characters of the file
+f = open("demofile.txt", "r")
+print(f.read(5))
+
+# Read Lines
+# You can return one line by using the readline() method
+# Read one line of the file:
+f = open("demofile.txt", "r")
+print(f.readline())
+
+# By looping through the lines of the file, you can read the whole file, line by line
+f = open("demofile.txt", "r")
+for x in f:
+  print(x)
+  
+# Close Files
+# It is a good practice to always close the file when you are done with it
+f = open("demofile.txt", "r")
+print(f.readline())
+f.close()
+
+
+######################################################################################################################################################
+
+# Write to an Existing File
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
