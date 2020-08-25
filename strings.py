4be97fbef064289460b3b9e5613f6d48b812a375
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-08-25 19:11:20 -0400

    Update strings.py

--- strings.py
+++ strings.py
@@ -46,6 +46,39 @@ print(myorder.format(quantity, itemno, price))
 # \t is tab     (indent key)
 
 
+#################################################################################################################################################
+
+# STRING FORMATTING
+# The format() method allows you to format selected parts of a string.
+# Sometimes there are parts of a text that you do not control, maybe they come from a database, or user input?
+# To control such values, add placeholders (curly brackets {}) in the text, and run the values through the format() method
+price = 49
+txt = "The price is {} dollars"
+print(txt.format(price))
+
+# Format the price to be displayed as a number with two decimals
+txt = "The price is {:.2f} dollars"
+
+# Multiple Values
+# If you want to use more values, just add more values to the format() method
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Index Numbers
+# You can use index numbers (a number inside the curly brackets {0}) to be sure the values are placed in the correct placeholders
+quantity = 3
+itemno = 567
+price = 49
+myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Named Indexes
+# You can also use named indexes by entering a name inside the curly brackets {carname}, 
+# but then you must use names when you pass the parameter values txt.format(carname = "Ford")
+
 
 """
 
