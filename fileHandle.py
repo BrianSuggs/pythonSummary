0f01b1670ea1361d32c3d2e56a9875ca76f67866
BrianSuggs <55091719+BrianSuggs@users.noreply.github.com>
2020-08-25 21:04:57 -0400

    Update fileHandle.py

--- fileHandle.py
+++ fileHandle.py
@@ -78,20 +78,67 @@ f.close()
 
 # Write to an Existing File
 
+# To write to an existing file, you must add a parameter to the open() function:
+"""
+"a" - Append - will append to the end of the file
+"w" - Write - will overwrite any existing content
+"""
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
+
+# Open the file "demofile3.txt" and overwrite the content
+f = open("demofile3.txt", "w")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile3.txt", "r")
+print(f.read())
 
 
+# Create a New File
+# To create a new file in Python, use the open() method, with one of the following parameters:
+"""
+"x" - Create - will create a file, returns an error if the file exist
+"a" - Append - will create a file if the specified file does not exist
+"w" - Write - will create a file if the specified file does not exist
+"""
+
+# Create a file called "myfile.txt"
+# the result is a new empty file
+f = open("myfile.txt", "x")
+
+# Create a new file if it does not exist
+f = open("myfile.txt", "w")
 
 
+######################################################################################################################################################
 
+# DELETING FILES
 
+# To delete a file, you must import the OS module, and run its os.remove() function
+import os
+os.remove("demofile.txt")
 
-
-
-
-
-
-
-
+# Check if a file exist
+# To avoid getting an error, you might want to check if the file exists before you try to delete it
+# Check if file exists, then delete it
+import os
+if os.path.exists("demofile.txt"):
+  os.remove("demofile.txt")
+else:
+  print("The file does not exist")
+  
+# Delete folder
+# To delete an entire folder, use the os.rmdir() method
+import os
+os.rmdir("myfolder")
 
 
 
